09d12c2def95f4a717817f891fc01bbc7dab110b
4ezy <aksonov10@gmail.com>
2017-10-25 16:18:18 +0300

    Добавлено окно авторизации, внесены мелкие изменения отображения главного окна.

--- MailClient/AuthWindow.xaml.cs
+++ MailClient/AuthWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MailClient
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AuthWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            AuthWindow authWindow = new AuthWindow() { Owner = this };
-            if (authWindow.ShowDialog() == false)
-                this.Close();
         }
     }
 }
